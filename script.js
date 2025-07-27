3e096a6974f1e96ee65c70f8a99015f3b933f43e
Bhoytems <temidayosodiq02@gmail.com>
2025-07-27 16:30:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Social media links
     const socialLinks = {
-        instagram: "https://instagram.com",
-        tiktok: "https://tiktok.com",
-        twitter: "https://x.com"
+        instagram: "https://www.instagram.com/volublemanof9ja?igsh=Mjdib3RydmplZXVo",
+        tiktok: "https://www.tiktok.com/@volublemanof9ja?_r=1&_d=ei3al4f7h6ljib&sec_uid=MS4wLjABAAAAWoJtQlXVudHJ0WjFM5AZR37lb_xds75zkf8k6aTl5IdRH35urbgxSZiWSdyfSkog&share_author_id=7440964022947759159&sharer_language=en&source=h5_m&u_code=ehcb994k38dd08&timestamp=1753629466&user_id=7440964022947759159&sec_user_id=MS4wLjABAAAAWoJtQlXVudHJ0WjFM5AZR37lb_xds75zkf8k6aTl5IdRH35urbgxSZiWSdyfSkog&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_iid=7454575476699907845&share_link_id=d1d6603e-81ff-40ef-89dc-2078a9f1596b&share_app_id=1233&ugbiz_name=ACCOUNT&social_share_type=5&enable_checksum=1",
+        twitter: "https://x.com/Volublemanof9ja?s=09"
+        support: "https://www.profitableratecpm.com/gi6n5d2c?key=0f8b2aa1c7882ffec8c32dd1e3bde665"
     };
     
     // Add click events to buttons
